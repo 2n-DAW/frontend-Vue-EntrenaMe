6fc4cd81a1de9cb1a9869ccb2a3523280baeab23
Llorens19 <llorenssorianodiego@gmail.com>
2024-11-22 17:33:39 +0100

    Test cors

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
     <div class="flex">
       <router-view class="flex-1 p-6"></router-view>
     </div>
+    <TestServer />
   </div>
+
 </template>
 
 
@@ -15,12 +17,14 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Header from './components/Header.vue';
+import TestServer from './components/TestServer.vue';
 
 
 export default defineComponent({
   name: 'App',
   components: {
     Header,
+    TestServer
   },
 });
 </script>
