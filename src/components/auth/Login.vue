c16eea9f0cc82c2b06d31f0c49e3aed73143256e
Llorens19 <llorenssorianodiego@gmail.com>
2024-12-03 17:00:36 +0100

    Validación login funcionando

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,19 +1,35 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref } from 'vue';
 import TextInputForm from '../text_input/TextInputForm.vue';
+import { emailRegex } from '../../shared/utils/Regex/emailRegex.util';
+import { passwordRegex } from '../../shared/utils/Regex/passwordRegex.util';
 
 
-const email_data = ref('');
-const password_data = ref('');
+const email_data = ref<String>('');
+const password_data = ref<String>('');
+const error_email = ref<String>('');
+const error_password = ref<String>('');
 
-watch([email_data, password_data], (new_value) => {
-    console.log(new_value);
-});
 
-const emit = defineEmits(['login']);
 const submitLogin = () => {
-    emit('login', { email: email_data.value, password: password_data.value });
+    console.log(validateLogin());
+    
 };
+
+const validateLogin = ():boolean=>{
+    let correct:boolean = true;
+    const email:String = email_data.value;
+    const password:String = password_data.value;
+
+    error_email.value = emailRegex(email);
+    error_password.value = passwordRegex(password);
+    
+    if(error_email.value ||error_password.value){
+        correct = false;
+    }
+    
+    return correct;
+}
 
 
 
@@ -28,6 +44,7 @@ const submitLogin = () => {
             id="email_input_login" 
             v-model:data="email_data" 
             placeholder="ejemplo123@email.com" 
+            :error= "error_email"
         />
         
         <TextInputForm 
@@ -36,6 +53,7 @@ const submitLogin = () => {
             id="password_input_login" 
             v-model:data="password_data" 
             placeholder="contraseña1234" 
+            :error="error_password"
         />
         
         
